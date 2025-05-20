2ef8a10850fc451a0810ed44f68574b64c379ab4
uzairFayaz <uzairfayaz33@gmail.com>
2025-05-20 10:31:37 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Vue</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/welcome',function (){
+    return view('welcome');
+})->name('welcome');
